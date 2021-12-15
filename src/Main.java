fbfe44cbf390c71b27075f04c6e8f08b6e139ef9
ElvisAlfaro <eljo_aa@hotmail.com>
2021-12-15 18:51:46 -0500

    First Commit
    Initialize project

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main (String [] args) {
+        //Aquí las instrucciones de inicio y control del programa
+        System.out.println ("Empezamos la ejecución del programa");
+    }
+}
